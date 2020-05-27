dce52be2ccc64ecc31cb0dd81b7c5e6b3b22a23d
makhloufi <makhloufi200@gmail.com>
2020-05-27 15:56:25 +0100

    initial commit

--- veterinaryapp/settings.py
+++ veterinaryapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't-+cfr3(0a4vh)&0$@4k+p2qsq-nms^et-egrgp-k@ntmt!$sp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
